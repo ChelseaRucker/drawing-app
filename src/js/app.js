66aed082f03b41eb0fa0d6064e930b8ba64d3000
ChelseaBurns <co.designux@gmail.com>
2015-10-14 13:07:37 -0500

    added add color button feature

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ var color = $(".selected").css("background-color");
 
 
 //When clicking on control list items
-$(".controls li").click(function() {
+$(".controls").on("click", "li", function() {
   //Deselect sibling elements
   $(this).siblings().removeClass("selected");
   //Select clicked element
@@ -20,7 +20,7 @@ $("#revealColorSelect").click(function() {
 	$("#colorSelect").toggle();
 });
 
-//Update new color span
+//Update "New color" span
 function changeColor() {
 	var r = $("#red").val();
 	var g = $("#green").val();
@@ -28,5 +28,20 @@ function changeColor() {
 	$("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
 }
 
-	//when color sliders change
-	$("input[type=range]").change(changeColor);
+//When color sliders change
+$("input[type=range]").change(changeColor);
+
+//When "Add color" is pressed
+$("#addNewColor").click(function() {
+	//Append the color to the controls ul
+	var $newColor = $("<li></li>");
+	$newColor.css("background-color", $("#newColor").css("background-color"));
+	$(".controls ul").append($newColor);
+
+	//Select the new color
+	$newColor.click();
+});
+
+
+//On mouse events on the canvas
+	//Draw lines
